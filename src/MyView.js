16329857d43ee9faf25e7a758eceea4036311ea0
guimingsu <2048396401@qq.com>
2017-01-07 21:27:23 +0800

    use react-redux
    use react-redux

--- src/MyView.js
+++ src/MyView.js
@@ -1,84 +1,57 @@
 import React,{ Component } from 'react';
-import { createStore,combineReducers } from 'redux'
+import {connect} from 'react-redux'
 
-// 生成新对象
- // Object.assign({}, state, { action });
- //  { ...state, ...action };
-
-function numReducer(state = 0, action) {
-    switch (action.type) {
-        case 'NUM':
-           return state + action.num;
-        default:
-            return state
-    }
-}
-
-function stringReducer(state = [], action) {
-    switch (action.type) {
-        case 'STRING':
-            return state.concat([action.msg]);
-        default:
-            return state
-    }
-}
-
-// 集合多个reducer用于初始化store,这里只有一个numReducer
-const rootReducer = combineReducers({
-    numReducer, //相当于一个同名的 key：value, 通过 store.getState().numReducer 获取对应的state
-    stringReducer,
-});
-
-let store = createStore(rootReducer);
-var unsubscribe;
-
-export default class MyView extends Component{
-
-      constructor(props) {
-        super(props);
-        this.state = {
-            num: store.getState().numReducer,//获取numReducer对应的state
-            array: store.getState().stringReducer,
-        };
-      }
-
-    componentWillMount() {
-        //注册监听
-        unsubscribe =  store.subscribe(this.numChanged.bind(this));
-    }
-
-    componentWillUnMount() {
-        //取消监听
-        unsubscribe();
-    }
-
-    numChanged(){
-          this.setState({
-              num: store.getState().numReducer,//获取numReducer对应的state
-              array: store.getState().stringReducer,
-          });
-      }
-
+class View extends Component{
       render(){
           return(
               <div>
-                  {this.state.num}
-                  <button onClick={this.numTaped.bind(this)} >num Tap</button>
-                  <button onClick={this.stringTaped.bind(this)} >String Tap</button>
-                  {this.state.array}
-
+                  {this.props.num}
+                  <button onClick={()=>this.props.onNumClick(3)} >num Tap</button>
+                  <button onClick={()=>this.props.onStringClick('hello ~')} >String Tap</button>
+                  {this.props.array}
               </div>
           );
       }
-
-    numTaped(){
-        store.dispatch(numAction(2))
-      }
-
-    stringTaped(){
-        store.dispatch(stringAction('hello ~'))
-    }
 }
+
+//mapStateToProps会订阅 Store，每当state更新的时候，就会自动执行
+//mapStateToProps的第一个参数总是state对象，还可以使用第二个参数ownProps，代表容器组件的props对象
+const mapStateToProps = (state) => ({
+    num: state.numReducer,
+    array: state.stringReducer,
+});
+
+//mapDispatchToProps 方式1. 用对象
+const mapDispatchToProps =  ({
+    onNumClick: numAction,
+    onStringClick: stringAction,
+});
+
+//mapDispatchToProps 方式2. 用方法
+// const mapDispatchToProps = (dispatch,ownProps) => {
+//     return {
+//         onNumClick: () => {
+//             dispatch({
+//                 type: 'NUM',
+//                 num: 1,
+//             });
+//         },
+//
+//         onStringClick: () => {
+//             dispatch({
+//                 type: 'STRING',
+//                 msg: 'hello ~',
+//             });
+//         },
+//     };
+// };
+
+const MyView = connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(View);
+
+export default MyView;
 
 function numAction(num) {
     return {
--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MyView from './MyView';
+import {Provider} from 'react-redux'
+import { createStore,combineReducers } from 'redux'
+
+function numReducer(state = 0, action) {
+    switch (action.type) {
+        case 'NUM':
+            return state + action.num;
+        default:
+            return state
+    }
+}
+
+function stringReducer(state = [], action) {
+    switch (action.type) {
+        case 'STRING':
+            return state.concat([action.msg]);
+        default:
+            return state
+    }
+}
+
+const rootReducer = combineReducers({
+    numReducer,
+    stringReducer,
+});
+
+const store  = createStore(rootReducer);
 
 ReactDOM.render(
-  <MyView />,
+    <Provider store={store}>
+        <MyView />
+    </Provider>,
   document.getElementById('root')
 );
